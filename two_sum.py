8e3ceddf6aa9df2bd734bdf4eb5a81bf1a93af09
Miguel Almeida <miguel.almeida1009@gmail.com>
2023-03-07 15:08:36 -0300

    Two Sum explanation

--- two_sum.py
+++ two_sum.py
@@ -1,12 +1,21 @@
 def two_sum(nums: list, target: int) -> list:
-    values = {}
+    values = {}  # Armazena os valores
 
     for i, elem in enumerate(nums):
+        # Cria uma variável comp (complementar) que faz a subtração
+        # do target pelo atual elemento sendo iterado na lista de 
+        # números.
         comp = target - elem
 
         if comp in values:
+            # Se o resultado da subtração (comp) estiver no dicionário
+            # values, então o resultado da faunção é o atual indice da
+            # iteração "i" e o indice armazenado no dicinionário values.
             return [values[comp], i]
 
+        # Caso o resultado da subtração (comp) não seja encontrado no
+        # dicionário values, então o atual indice é adicionado ao dicionário
+        # como o valor da chave do atual elemento.
         values[elem] = i
 
     return []
